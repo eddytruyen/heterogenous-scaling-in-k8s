f33c2332eeacb400368d525dab837f02314f95db
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2020-10-13 15:45:10 +0000

    update-api

--- charts/update_api-versions.sh
+++ charts/update_api-versions.sh
@@ -1,2 +1,3 @@
 for i in `find . | grep 'statefulset\|deployment\.yaml'`; do sed -i 's/extensions\/v1beta1/apps\/v1/g' $i; done
 for i in `find . | grep 'statefulset\|deployment\.yaml'`; do sed -i 's/apps\/v1beta2/apps\/v1/g' $i; done
+for i in `find . | grep '.*riority.*\.yaml'`; do sed -i 's/scheduling.k8s.io\/v1beta1/scheduling.k8s.io\/v1/g' $i; done
