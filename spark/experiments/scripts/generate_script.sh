ab44fe4cd18bf535e6574e5bbb3c53f81ae8fa50
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-01-06 16:58:34 +0000

    update

--- spark/experiments/scripts/generate_script.sh
+++ spark/experiments/scripts/generate_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 nrOfTenants=$1
-partitions=${2:-0}
-tenantGroup=${3:-2}
-outputFile=${4:-output.conf}
+executorMemory=${2:-0}
+partitions=${3:-0}
+tenantGroup=${4:-2}
+outputFile=${5:-output.conf}
 
 if [ -f "$outputFile" ]
 then
@@ -12,6 +13,12 @@ fi
 
 iterations=$(($nrOfTenants - 1))
 cat header >> tmp
+if [ $executorMemory -gt 0 ]
+then
+  executorMemoryOverhead=$((($executorMemory / 10) * 1024))
+  sed -i "s/executor-memory = \".*\"/executor-memory = \"${executorMemory}G\"/g" tmp
+  sed -i "s/spark.executor.memoryOverhead = \".*\"/spark.executor.memoryOverhead = \"$executorMemoryOverhead\"/g" tmp
+fi
 for i in `seq $iterations`
 do
 #  if [ $i -lt $nrOfTenants ]
