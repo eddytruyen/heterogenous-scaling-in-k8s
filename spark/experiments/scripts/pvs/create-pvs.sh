d9c26492cecc4c1b37484f434a522f27080d9c12
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-02-06 19:52:40 +0000

    update

--- spark/experiments/scripts/pvs/create-pvs.sh
+++ spark/experiments/scripts/pvs/create-pvs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+current_dir=`pwd`
+scriptdir="$(dirname "$0")"
+cd "$scriptdir"
 namespace=${1:-default}
 min_nodes=${2:-1}
 echo $namespace
@@ -8,3 +10,4 @@ count=`kubectl get nodes | wc -l`
 nodes=$(( $count - ( $min_nodes + 1)))
 for i in `seq $nodes`; do sed "s/local-pv/local-pv-$namespace-$i/g" persistentvolume.yaml > pv.yaml; kubectl create -f pv.yaml; done
 rm pv.yaml
+cd "$current_dir"
--- spark/experiments/scripts/pvs/delete-pvs.sh
+++ spark/experiments/scripts/pvs/delete-pvs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+current_dir=`pwd`
+scriptdir="$(dirname "$0")"
+cd "$scriptdir"
 namespace=${1:-default}
 release=${2:-my-release}
 min_nodes=${3:-1}
@@ -12,3 +14,4 @@ kubectl delete pvc spark-data-${release}-master-0 -n $namespace
 for i in `seq  $nodes`; do y=$(( i-1 )); kubectl delete pvc spark-data-${release}-worker-$y -n $namespace; done
 for i in  `seq $nodes`; do  sed "s/local-pv/local-pv-${namespace}-$i/g" persistentvolume.yaml > pv.yaml; kubectl delete -f pv.yaml; done
 rm pv.yaml
+cd "$current_dir"
