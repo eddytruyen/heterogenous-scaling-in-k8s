7b65a07850d9d76ac580b906065cb35cb4f8231b
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-02-10 10:04:05 +0000

    run-bench.sh

--- spark/experiments/scripts/run-bench.sh
+++ spark/experiments/scripts/run-bench.sh
@@ -1,11 +1,19 @@
 current_dir=`pwd`
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
+
+namespace=$1
+releasename=$2
+workload=$3
+tenantgroup=$4
+outputdir=$5
+rm -r results-$workload-g$tenantgroup-t-*.csv
 mr=`cat /etc/podinfo/mem_request`
 exec_overhead_gb=$(($mr / 10))
 exec_mem=$(($mr - ${exec_overhead_gb}))
-sed "s/executor-memory = \".*G\"/executor-memory = \"${exec_mem}G\"/g" examples/output.conf > examples/tmp.conf
+sed "s/executor-memory = \".*G\"/executor-memory = \"${exec_mem}G\"/g" output.conf | sed "s/Release-master-svc.Namespace/$releasename-master-svc.$namespace/g" > tmp.conf
 exec_overhead_mb=$((${exec_overhead_gb} * 1024))
-sed  -i "s/spark.executor.memoryOverhead = \".*\"/spark.executor.memoryOverhead = \"${exec_overhead_mb}\"/g" examples/tmp.conf
-./bin/spark-bench.sh examples/tmp.conf
+sed  -i "s/spark.executor.memoryOverhead = \".*\"/spark.executor.memoryOverhead = \"${exec_overhead_mb}\"/g" tmp.conf
+./bin/spark-bench.sh tmp.conf  2> run.log
+cp -r results-$workload-g$tenantgroup-t-*.csv $outputdir
 cd "$current_dir"
