3b69f2096fd53235e2f79c4d24b288ddf6a491df
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-03-29 19:02:58 +0200

    update

--- spark/experiments/scripts/R/run.R
+++ spark/experiments/scripts/R/run.R
@@ -9,6 +9,7 @@ library(mosaic)
 library(mosaicCore)
 library(mosaicData)
 library(mosaicCalc)
+library(data.table)
 
 source(file="get_files.R")
 
@@ -68,19 +69,35 @@ plotHighestDeployment<-function(workload, deployment, metric, operation, cropLen
 }
 
 plotHighestVpaDeployment<-function(workload, deployment, metric, operation, cropLength, title, xl, yl, extend_y_lim=FALSE, y_lim_addition = 0, scalefactor=5, pc, lt, color="black")  {
-  data=get(metric, get(deployment, get(workload, workloads)))
+  w2=get(deployment, get(workload, workloads))
+  data=get(metric,w2)
+  nb_of_tenants<-names(data$total)
+  pod_replicas<-unname(w2$pod_replicas)
+  cpus<-unname(w2$cpus)
+  memGB<-unname(w2$mem)
+  plist <- vector("list", length(pod_replicas))
+  for (i in 1:length(pod_replicas)) {
+    plist[[i]] <- c(paste(pod_replicas[i],"r", sep=""),paste(cpus[i],"c",sep=""))
+  }
   z=unlist(get(operation, data))
   z=z[1:cropLength]
   x=(1:cropLength)*scalefactor
-  x2=names(get("raw", get(deployment, get(workload, workloads))))
-  x3=rbind(x,x2)
+  x2=names(get("total",get("median", get(deployment, get(workload, workloads)))))
+  #x3=rbind(x,x2)
+  #frame=data.frame(x2, unname(z))
   #fit <- lm(z ~ x + I(x^2))
   if (extend_y_lim == TRUE) { 
     y_lim=min(unlist(z))-y_lim_addition
-    plot(z~x3, main=title, xlab=xl,ylab=yl, xaxp  = c(1,length(x),length(x)-1),pch=pc, lty=lt, col=color, ylim=c(0, y_lim))
+    plot(axes=FALSE, z~x, main=title, xlab=xl,ylab=yl, xaxp  = c(1,length(x),length(x)-1),pch=pc, lty=lt, col=color, ylim=c(0, y_lim))
+    #plotPoints(z~x2)
   } else {
-    plot(z~x3, main=title, xlab=xl,ylab=yl, pch=pc, lty=lt, col=color)
+    plot(z~x, axes=FALSE,main=title, xlab=xl,ylab=yl, pch=pc, lty=lt, col=color)
+        #plotPoints(z~x2)
   }
+  axis(2)
+  axis(1, at=seq_along(z),labels=as.character(nb_of_tenants), las=1)
+  axis(3, at=seq_along(z),labels=as.character(plist),las=1)
+  box()
   lines(x, z, pch=pc, lty=lt, col=color)
   return(data)
 }
@@ -118,6 +135,27 @@ plotReplicas<-function(workload, deployment, cropLength, pc, lt, scalefactor=5, 
 workloads=checkInputWorkloads()
 
 
+L=10
+metric="mean"
+workload="spark-bench"
+deployment="vps"
+
+
+synthesize<-function(workload, deployment, metric, cropLength)  {
+  operations=get(metric, get(deployment, get(workload, workloads)))
+  total <- unname(unlist(operations[1]))
+  for (op in operations[2:length(operations)]) {
+      total <- total + unlist(op)
+  }
+  
+ return(total)
+}
+
+workloads[[workload]][[deployment]]$mean[["total"]] <- synthesize(workload,deployment,"mean",L)
+workloads[[workload]][[deployment]]$median[["total"]] <- synthesize(workload,deployment,"median",L)
+
+    
+
 runs = get_sorted_runs(directory)
 
 for (j in operations) {
@@ -149,13 +187,24 @@ workloads$`spark-bench`$`hpa-100cpu`$pod_replicas <- replicas
 replicas <- c(1,1,1,1,2,3,4,5,7,9) #auto-hpa-11Gimem
 workloads$`spark-bench`$`hpa-11Gimem`$pod_replicas <- replicas
 
+replicas <-c(2,2,2,4,4,5,3) #vps
+workloads$`spark-bench`$`vps`$pod_replicas <- replicas
+cpus <- c(1,2,3,2,3,2,4)
+workloads$`spark-bench`$`vps`$cpus <- cpus
+mems <- c(7,14,30,14,14,14,30)
+workloads$`spark-bench`$`vps`$mems <- mems
+
+
 
 workloads = readRDS(file=workloadfile)
-L=10
-metric="mean"
+
+
+
+L=7
+metric="median"
 workload="spark-bench"
-deployment="hps"
-operation=operations[1]
+deployment="vps"
+operation="total"
 title="Horizontal auto-scaling cpu-threshold 80%"
 xl="Number of tenants"
 #yl=paste("95th ", metric," of response latency (ms)", sep="");
@@ -165,35 +214,64 @@ data=(get(metric, get(deployment, get(workload, workloads))))
 y=unlist(get(operation, data))
 y=unname(y[1:L])
 print(y)
+
+
 z = (1:L)
 frame=data.frame(z,y)
 
+#f <- nls(y ~ A * exp(B * z) + C, data = frame, start = start)
+
+
 computeFunction <- function(y,z,frame) {
-  f = fitModel(y~(1-A*z - B*log(z))/C, data = frame)
+  c <- min(y)*0.5
+  model.0 <- lm(log(y - c) ~ z, data=frame)
+  start <- list(A=exp(coef(model.0)[1]), B=coef(model.0)[2], C=c)
+  f = fitModel(y ~ A * exp(B * z) + C, data = frame, start = start)
   return(f)
 }
 
 f <- computeFunction(y,z,frame)
 
+z=(1:L)
 plotPoints(y~z)
 coef(f)
-plotFun(f(z)~z, z.lim = range(1:L),add=TRUE, col="blue")
+plotFun(f(z)~z, z.lim = range(1:L), add = TRUE, col="blue")
+z=(1:100)
+plotFun(f(z)~z, z.lim = range(1:100), add = FALSE, col="blue")
 
 d=D(f(z)~z)
-soln=integrateODE(dz~f(z),z=1,tdur=10)
-plotFun(soln$z, tlim=range(1,10))
+
+soln=integrateODE(dz~f(z),z=1,tdur=100)
+plotFun(soln$z, tlim=range(1,100))
 
 
 d(1)
 d(2)
 f(1)-(d(2)-d(1))
+
+
+L=7
+metric="median"
+workload="spark-bench"
+deployment="vps"
+operation=operations[1]
+title="Horizontal auto-scaling cpu-threshold 80%"
+xl="Number of tenants"
+#yl=paste("95th ", metric," of response latency (ms)", sep="");
+yl=paste("0.95 quantile", " of job completion time (s)", sep="");
+
+
+
 pch1 = c(9,19,18,2,25) 
 lty1 = c(1,6,25,29,134) 
 col1 = c(25,134,450,25,6)
 
+#plotPoints(unlist(unname(workloads$`spark-bench`$vps$mean$`select * from parquet`))~(1:7))
 
-data=plotHighestDeployment(workload, deployment, metric, operation, L, title, xl, yl, pc=pch1[1], lt=lty1[1], 
-                           color=col1[1], extend_y_lim = TRUE, y_lim_addition = -80, scalefactor = 1)
+
+
+data=plotHighestVpaDeployment(workload, deployment, metric, operation, L, title, xl, yl, pc=pch1[1], lt=lty1[1], 
+                           color=col1[1], extend_y_lim = TRUE, y_lim_addition = -150, scalefactor = 1)
 #redish
 
 plotOtherDeploymentasLine(data, operations[2], L, pch1[2], lty1[2], color=col1[2], scalefactor = 1)
