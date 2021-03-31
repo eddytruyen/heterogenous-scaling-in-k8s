203fda3a93494965c610a12ab7584601e54d8640
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-03-31 08:57:12 +0000

    update

---
+++ apps/matrix-generator/store-result.sh
@@ -0,0 +1,10 @@
+#$1 directory exp
+#$2 namespace/sla name
+#$3 number of tenants to be updated
+
+x=`pwd`
+cd Results/$1/$2/
+for i in `seq $3`; do for j in 0 1; do cat ${i}_tenants-ex${j}/op/results.json >> results1.json; done; done
+sed "s/false}}\]\[{\"bench/false}},{\"bench/g" results1.json | sed "s/true}}\]\[{\"bench/true}},{\"bench/g"  > results2.json
+cd $x
+
