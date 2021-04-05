3b737480e821263bf75b4bafb4073cb0fbc84d4c
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-04-05 15:22:21 +0000

    update

--- apps/matrix-generator/test.py
+++ apps/matrix-generator/test.py
@@ -2,7 +2,7 @@ import argparse
 import textwrap
 import yaml
 from src.generator import generate_matrix2 as generate_matrix2
-
+from src.searchwindow import ScalingFunction
 
 parser = argparse.ArgumentParser(
 
@@ -30,6 +30,18 @@ config_data = yaml.safe_load(open(CONFIG_FILE))
 
 
 def generate_matrix():
-	generate_matrix2(config_data)
+	#generate_matrix2(config_data)
+	f=ScalingFunction(172.2835754,-0.4288966,66.9643290,2,14)
+	print(f.target(150,10))
+	max=f.maximum(1,10)
+	min=f.minimum(1,10)
+	range=max-min
+	#for x in range(1,10,1):
+	#	if  
+	#for i in range(100,110,1):
+	#	print(f.eval(i))
+	print(f.maximum(1,10))
+	print(f.minimum(1,10))
+	print(f.derivative(1,2))
 
 generate_matrix()
