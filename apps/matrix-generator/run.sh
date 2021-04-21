1d3669500a8add6805adb14154bcf3ea1060a175
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-04-21 20:10:17 +0000

    update

--- apps/matrix-generator/run.sh
+++ apps/matrix-generator/run.sh
@@ -1,10 +1,12 @@
-for i in 4 5 6 7 8 9
+for i in `seq 10`
  do 
   sed "s/indow: 1/indow: $i/g" conf/matrix-spark.yaml > conf.yaml
   #x=expr $i-1 
   #echo $x
   #mv nohup.out nohup-searchwindow$x.out
   python -u matrix.py conf.yaml > nohup-searchwindow$i.out
-  cp Results/matrix.yaml data/matrix-spark-150-searchwindow$i.yaml
+  cp Results/matrix.yaml data/matrix-spark-150-b-searchwindow$i.yaml
+  ./store-report.sh exp3 silver 
+  echo `wc -l Results/exp3/silver/reports.csv` >> samples 
   #mv nohup.out nohup-searchwindow$i.out
  done
