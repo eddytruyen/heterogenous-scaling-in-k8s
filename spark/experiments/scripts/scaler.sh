4c1f91d5bc91a76cb9a028dd48c3fa9cbbfbc65e
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-04-29 15:02:02 +0000

    update

--- spark/experiments/scripts/scaler.sh
+++ spark/experiments/scripts/scaler.sh
@@ -7,7 +7,9 @@ namespace=${4:-silver}
 workload=${5:-sql}
 lastTenantId=$((($nrofTenants - 1) + $startingTenantId))
 tenantGroup=2
-clientmode=`grep '\/\/deploy' header | wc -l` 
+clientmode=`grep '\/\/deploy' $workload/header | wc -l` 
+kubectl create -f spark-client/ -n $namespace
+kubectl wait --for=condition=Ready  pod/spark-client-0 -n $namespace  --timeout=120s
 for i in `seq $startingTenantId $lastTenantId`
 do
   #nrOfPartitions=$(($i * 2))
@@ -15,7 +17,7 @@ do
   #2 cores per tenant
   ./rescale.sh $namespace $i
   #if [ $clientmode -eq 0 ]
-  #then
+  #then`
   #  replicas=$(($i + 1))
   #  #one replica is added for the driver
   #else
@@ -44,3 +46,4 @@ do
   period=$(($t2 - $t1))
   #if [ $period -lt 120 ]; then echo "sleeping for 5400 sec"; sleep 5400; fi	 
 done
+kubectl delete  -f spark-client/ -n $namespace
