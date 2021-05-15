bf717c34b5520a98c85bf1640a8a959ff13372c2
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-05-15 13:12:41 +0000

    update

--- apps/matrix-generator/run.sh
+++ apps/matrix-generator/run.sh
@@ -5,11 +5,11 @@ for i in `seq 1 10`
   #x=expr $i-1 
   #echo $x
   #mv nohup.out nohup-searchwindow$x.out
-  python -u matrix.py conf.yaml > nohup-searchwindow-tc3b-$i.out
-  cp Results/matrix.yaml data/matrix-spark-150-tc3-searchwindow$i.yaml
+  python -u matrix.py conf.yaml > nohup-searchwindow-tc2-$i.out
+  cp Results/matrix.yaml data/matrix-spark-150-tc2-shrdrpl2-searchwindow$i.yaml
   ./store-report.sh exp3 silver 
-  echo `wc -l Results/exp3/silver/reports.csv` >> samples-tc3b
- ./store-result.sh exp3 silver
-  mv Results/exp3/silver/results3.json dataset/results-tc3-$i.json 
+  echo `wc -l Results/exp3/silver/reports.csv` >> samples-tc2-shrdrpl2
+# ./store-result.sh exp3 silver
+#  mv Results/exp3/silver/results3.json dataset/results-tc3-$i.json 
   #mv nohup.out nohup-searchwindow$i.out
  done
