04d345d02243147a593875e60cb00086b3f44dde
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-06-02 09:27:26 +0000

    update

--- apps/matrix-generator/src/searchwindow.py
+++ apps/matrix-generator/src/searchwindow.py
@@ -302,7 +302,7 @@ class AdaptiveScaler:
                                 return conf_cost-total_cost - 1
                         else:
                                 print("SCALE UP DIFF")
-                                return 3 #total_cost + 1 - conf_cost
+                                return total_cost + 1 - conf_cost
 
 		def is_worker_scaleable(worker_index):
                         nonlocal scale_down
