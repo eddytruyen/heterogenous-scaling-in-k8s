d598808594004f7038aabe3cb830f7d16e41fea2
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-06-07 14:41:24 +0000

    update

--- spark/experiments/scripts/run.sh
+++ spark/experiments/scripts/run.sh
@@ -2,26 +2,28 @@
 
 nrofTenants=$1
 startingTenantId=${2:-1}
-executorMemory=${3:-0}
+executorMemory=${3:-2}
 namespace=${4:-silver}
 workload=${5:-sql}
+release=${6:-silver-spark}
 lastTenantId=$((($nrofTenants - 1) + $startingTenantId))
 tenantGroup=2
-clientmode=`grep '\/\/deploy' header | wc -l` 
+#clientmode=`grep '\/\/deploy' header | wc -l` 
+rm timings.csv 
 for i in `seq $startingTenantId $lastTenantId`
 do
   #nrOfPartitions=$(($i * 2))
   nrOfPartitions=0
   #2 cores per tenant
-  if [ $clientmode -eq 0 ]
-  then
-    replicas=$(($i + 1))
+  #if [ $clientmode -eq 0 ]
+  #then
+  #  replicas=$(($i + 1))
     #one replica is added for the driver
-  else
-     replicas=$i
-  fi
+  #else
+  replicas=$i
+  #fi
   echo "scaling to $replicas replicas..."
-  kubectl scale statefulset my-release-spark-worker --replicas=$replicas
+  kubectl scale statefulset $release-worker --replicas=$replicas
   echo "calculating sleeptime for $replicas replicas to come up"
   sleeptimeFor1Replica=60
   if [ $i -eq $startingTenantId ] 
@@ -37,8 +39,8 @@ do
   sudo cp ./$workload/output.conf /mnt/nfs-disk-2/spark-bench/
   echo "executing script for $i tenants"
   t1=`date +%s` 
-  kubectl exec -it -n $namespace $namespace-spark-worker-0 -- runuser -u spark spark_data/spark-bench/run-bench.sh $namespace $workload $tenantGroup 2> /tmp/test
+  kubectl exec -it -n $namespace spark-client-0 -- runuser -u spark spark_data/spark-bench/run-bench.sh $namespace $release $workload $tenantGroup 2> /tmp/test
   t2=`date +%s`
   period=$(($t2 - $t1))
-  if [ $period -lt 120 ]; then echo "sleeping for 5400 sec"; sleep 5400; fi	 
+  echo period >> timings.csv
 done
