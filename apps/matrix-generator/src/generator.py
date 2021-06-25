5ee65acc8bdce2ad7ff186ba3c09087663f868b2
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-06-25 14:06:56 +0000

    update

--- apps/matrix-generator/src/generator.py
+++ apps/matrix-generator/src/generator.py
@@ -279,6 +279,7 @@ def remove_failed_confs(sorted_combinations, workers, results, optimal_conf, sta
 def filter_samples(sorted_combinations, workers, start, window, previous_results, start_tenant, tenant_nb, check_workers=False, ScaledDownWorkerIndex=-1):
 	i=start_tenant
 	new_window=window
+	start_window=window
 	if previous_results:
 		while i <= tenant_nb:
 			previous_tenant_conf=get_conf(workers, previous_results[str(i)])
@@ -301,7 +302,7 @@ def filter_samples(sorted_combinations, workers, start, window, previous_results
 				else:
 					print("not removed")
 			if new_window == 0:
-				return filter_samples(sorted_combinations, workers, start+window, window, previous_results, start_tenant, tenant_nb, check_workers, ScaledDownWorkerIndex)
+				return filter_samples(sorted_combinations, workers, start+start_window, start_window, previous_results, start_tenant, tenant_nb, check_workers, ScaledDownWorkerIndex)
 			else:
 				i+=1
 				window=new_window
