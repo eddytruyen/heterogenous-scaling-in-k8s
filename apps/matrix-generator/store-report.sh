6b46db8cbde61ef3da7502e81cb409b407059d1e
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-06-29 13:39:52 +0000

    update

---
+++ apps/matrix-generator/store-report.sh
@@ -0,0 +1,16 @@
+#$1 directory exp
+#$2 namespace/sla name
+#$3 number of tenants to be updated
+
+x=`pwd`
+cd Results/$1/$2/
+rm reports.csv
+find . | grep report.csv > files
+y=$(head -n 1 files)
+cat $y > reports.csv
+sed -i 1d files
+for i in `cat files`; do echo $i; cp $i tmp-file; sed -i 1d tmp-file; cat tmp-file >> reports.csv; rm tmp-file; done
+sed -i '/^$/d' reports.csv
+rm files
+cd $x
+
---
+++ apps/matrix-generator/store-result.sh
@@ -0,0 +1,28 @@
+#$1 directory exp
+#$2 namespace/sla name
+
+x=`pwd`
+cd Results/$1/$2/
+rm results*.json
+for i in `find . | grep 'results.*-srt'`; do echo "removing $i"; rm $i; done
+for i in `find . | grep 'results\.json.*-cr'`; do echo "removing $i"; rm $i; done
+find . | grep results.json > files
+for i in `cat files`; do echo "cutting $i"; sed -z 's;false}},{"bench;false}}\n{"bench;g' $i | sed -z 's;true}},{"bench;true}}\n{"bench;g' > $i-cr ; done
+for i in `cat files`; do echo "sorting $i"; sort $i-cr -o $i-srt; done
+y=$(head -n 1 files)
+cat $y-srt > results1.json
+sed -i 1d files
+for i in `cat files`; do echo $i; comm -13 $y-srt $i-srt >> results1.json; done
+rm files
+sed  -z 's;false}}\n{"bench;false}},{"bench;g' results1.json |  sed -z 's;true}}\n{"bench;true}},{"bench;g' > results2.json
+sed  -z 's;false}}\]\n\[{"bench;false}},{"bench;g' results2.json |  sed -z 's;true}}\]\n\[{"bench;true}},{"bench;g' > results3.json
+sed  -z 's;false}}\n\[{"bench;false}},{"bench;g' results3.json |  sed -z 's;true}}\n\[{"bench;true}},{"bench;g' > results2.json
+sed  -z 's;false}}\]\n{"bench;false}},{"bench;g' results2.json |  sed -z 's;true}}\]\n{"bench;true}},{"bench;g' > results3.json
+cd $x
+echo "Storing aggregated results.json"
+mv Results/$1/$2/results3.json results.json
+echo "Moving the output of the matrix-generator to /tmp/matrix-generator-results"
+if [ ! -d /tmp/matrix-generator-results ] 
+	mkdir /tmp/matrix-generator-results
+rm -r /tmp/matrix-generator-results/*
+mv Results/$1/$2/* /tmp/matrix-generator-results
