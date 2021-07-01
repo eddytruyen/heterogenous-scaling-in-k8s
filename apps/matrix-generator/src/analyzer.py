bb6fcab54fbe59e16a4df7c184422d28e3a6c364
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-07-01 18:38:39 +0000

    update

--- apps/matrix-generator/src/analyzer.py
+++ apps/matrix-generator/src/analyzer.py
@@ -14,12 +14,17 @@ class ExperimentAnalizer:
 		runs=results[1:]
 		values=[]
 		score_index=header.index('score')
+                for run in runs:
+                        d={}
+                        for h,v in zip(header.split(),run.split()):
+                                d[h]=v
+                        values.append(d)
 
-		for run in runs:
-			d={}
-			for h,v in zip(header[:score_index+5].split(),run[:score_index+1].split()):
-				d[h]=v
-			values.append(d)  
+#		for run in runs:
+#			d={}
+#			for h,v in zip(header[:score_index+5].split(),run[:score_index+1].split()):
+#				d[h]=v
+#			values.append(d)  
 
 		return values
 
