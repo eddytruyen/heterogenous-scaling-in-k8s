b91b4ced751f2e6c5d8e0f1cd101f9669a58216a
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-07-02 16:58:48 +0000

    update

--- apps/matrix-generator/test.py
+++ apps/matrix-generator/test.py
@@ -32,11 +32,7 @@ config_data = yaml.safe_load(open(CONFIG_FILE))
 
 def generate_matrix():
 	generate_matrix2(config_data)
-	nodes=[[4,8],[8,32],[8,32],[8,32],[8,16],[8,16],[8,8],[3,6]]
-	f=ScalingFunction(667.1840993,-0.8232555,136.4046126,2,2,True,nodes)
-	for i in range(1,10,1):
-		print(f.eval(i))
-	print(f.target(90,1))
+	#print(f.target(250,1))
 #	max=f.maximum(2,3)
 #	print(max)
 #	min=f.minimum(1,10)
