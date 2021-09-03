86d3a7b0e38f9d3494ca1e2a6c8f4d15b75c7d32
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-09-03 19:43:21 +0000

    update

--- apps/matrix-generator/test2.sh
+++ apps/matrix-generator/test2.sh
@@ -8,14 +8,15 @@ curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=140
 sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=190&previoustenants=3&previousconf=0_1_0_1" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=160&previoustenants=3&previousconf=0_1_1_1" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=160&previoustenants=3&previousconf=0_1_0_2" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=190&previoustenants=3&previousconf=0_1_1_0" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=160&previoustenants=5&previousconf=0_1_1_1" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=120&previoustenants=3&previousconf=0_2_0_1" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=120&previoustenants=5&previousconf=0_2_0_1" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=190&previoustenants=5&previousconf=0_1_0_2" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=6&completiontime=120&previoustenants=5&previousconf=0_2_0_1" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=5&completiontime=160&previoustenants=5&previousconf=0_2_0_1" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=6&completiontime=190&previoustenants=6&previousconf=0_2_0_1" | jq '.'
 sleep 1
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=6&completiontime=190&previoustenants=6&previousconf=1_1_0_1" | jq '.'
 
