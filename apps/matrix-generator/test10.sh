734d768a8c9840056acd1e06ae9c4899aab30e3e
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-09-16 13:12:26 +0000

    update

--- apps/matrix-generator/test10.sh
+++ apps/matrix-generator/test10.sh
@@ -1,35 +1,35 @@
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_0_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=160&previoustenants=2&previousconf=0_1_1_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_2_0_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_1_0_2" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=140&previoustenants=2&previousconf=0_1_1_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=160&previoustenants=3&previousconf=0_1_1_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=120&previoustenants=3&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=4&completiontime=120&previoustenants=3&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=4&completiontime=120&previoustenants=4&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=4&completiontime=120&previoustenants=4&previousconf=0_2_1_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=4&completiontime=160&previoustenants=4&previousconf=0_1_2_0" | jq '.' > test.json
-sleep 10
+sleep 1
 echo '{
   "CompletionTime": "120",
   "SLAName": "silver",
@@ -53,15 +53,13 @@ echo '{
 cat test.json
 diff test.json test2.json
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=160&previoustenants=2&previousconf=0_1_1_0" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_1_0" | jq '.'
-sleep 10
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=4&completiontime=120&previoustenants=4&previousconf=0_1_1_1" | jq '.'
-sleep 10
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=140&previoustenants=2&previousconf=0_2_0_0" | jq '.' > test.json
-sleep 10
+sleep 1
 echo '{
   "CompletionTime": "140",
   "SLAName": "silver",
