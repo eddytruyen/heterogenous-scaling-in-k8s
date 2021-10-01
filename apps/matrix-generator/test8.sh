aae88a809b7e1a126122888088403ea6c999f018
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-10-01 11:35:26 +0000

    update

--- apps/matrix-generator/test8.sh
+++ apps/matrix-generator/test8.sh
@@ -9,6 +9,11 @@ sleep 3
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=90&previoustenants=2&previousconf=1_1_0_0" | jq '.'
 sleep 3
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=10&completiontime=190&previoustenants=10&previousconf=1_1_0_0" | jq '.'
+sleep 3
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=10&completiontime=120&previoustenants=10&previousconf=1_1_0_1" | jq '.'
+sleep 3
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=10&completiontime=12&previoustenants=10&previousconf=1_1_0_1" | jq '.'
+sleep 3
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=90&previoustenants=2&previousconf=1_1_0_0" | jq '.'
 sleep 3
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=90&previoustenants=3&previousconf=1_1_0_0" | jq '.'
