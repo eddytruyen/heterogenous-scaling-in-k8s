0768a2a2df0f428eb2b02f73030db79f13c1252a
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-10-01 19:00:25 +0000

    update

--- apps/matrix-generator/src/generator.py
+++ apps/matrix-generator/src/generator.py
@@ -885,8 +885,16 @@ def remove_failed_confs(sorted_combinations, workers, rm, results, slo, optimal_
  					rm.remove_sample_for_conf(failed_conf)
 		return next_index
 
+
 def has_different_workers_than(adaptive_scaler_a, conf_a, adaptive_scaler_b, conf_b):
-    return  has_smaller_workers_than(adaptive_scaler_a, conf_a, adaptive_scaler_b, conf_b) or  has_smaller_workers_than(adaptive_scaler_b, conf_b, adaptive_scaler_a, conf_a)
+    for worker_index, conf_pair in enumerate(zip(conf_a, conf_b)):
+        if conf_pair[0] > 0 and conf_pair[1] > 0:
+            if not adaptive_scaler_a.workers[worker_index].equals(adaptive_scaler_b.workers[worker_index]):
+                return True
+    return False
+
+#def has_different_workers_than(adaptive_scaler_a, conf_a, adaptive_scaler_b, conf_b):
+#    return  has_smaller_workers_than(adaptive_scaler_a, conf_a, adaptive_scaler_b, conf_b) or  has_smaller_workers_than(adaptive_scaler_b, conf_b, adaptive_scaler_a, conf_a)
 
 def has_smaller_workers_than(adaptive_scaler_a, conf_a, adaptive_scaler_b, conf_b):
     response=False
@@ -903,6 +911,8 @@ def has_smaller_workers_than(adaptive_scaler_a, conf_a, adaptive_scaler_b, conf_
 
 
 def is_smaller_worker_than(worker_a, worker_b):
+    if worker_a.equals(worker_b):
+        return False
     for res in worker_a.resources.keys():
         if worker_a.resources[res] > worker_b.resources[res]:
             return False
@@ -922,7 +932,7 @@ def tenant_nb_X_result_conf_conflict_with_higher_tenants(adaptive_scalers,previo
                 if int(t) > tenant_nb:
                         other_conf=get_conf(adaptive_scaler.workers, previous_results[t])
                         other_as=get_adaptive_scaler_for_tenantnb_and_conf(adaptive_scalers,adaptive_scaler,previous_results,int(t),other_conf,slo, clone_scaling_function=True, log=False)
-                        if float(previous_results[t]['CompletionTime']) < 1.0 or float(previous_results[t]['CompletionTime']) > float(slo) * 100 and has_different_workers_than(other_as, other_conf, adaptive_scaler, result_conf):
+                        if (float(previous_results[t]['CompletionTime']) < 1.0 or float(previous_results[t]['CompletionTime']) > float(slo) * 100) and has_different_workers_than(other_as, other_conf, adaptive_scaler, result_conf):
                             return True
                         elif float(previous_results[t]['CompletionTime']) <= slo and has_smaller_workers_than(other_as, other_conf, adaptive_scaler, result_conf):
                             return True
