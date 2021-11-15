6079bcb0b51d5a5d0e790992c9e20aff9606edab
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-11-15 12:44:48 +0000

    update

--- apps/matrix-generator/test12.sh
+++ apps/matrix-generator/test12.sh
@@ -6,6 +6,8 @@ curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=160
 sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_1_2_0" | jq '.'
 sleep 1
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=120&previoustenants=2&previousconf=0_2_1_0" | jq '.'
+sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=140&previoustenants=2&previousconf=0_1_1_0" | jq '.'
 sleep 1000
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3" | jq '.'
