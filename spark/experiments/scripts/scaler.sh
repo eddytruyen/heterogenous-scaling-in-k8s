ba5dd2a500f2f129a54c418456c42aae0b7029e2
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-11-16 13:28:03 +0000

    debug_spark_client

--- spark/experiments/scripts/scaler.sh
+++ spark/experiments/scripts/scaler.sh
@@ -8,8 +8,8 @@ workload=${5:-sql}
 lastTenantId=$((($nrofTenants - 1) + $startingTenantId))
 tenantGroup=2
 clientmode=`grep '\/\/deploy' $workload/header | wc -l` 
-kubectl create -f spark-client/ -n $namespace
-kubectl wait --for=condition=Ready  pod/spark-client-0 -n $namespace  --timeout=120s
+#kubectl create -f spark-client/ -n $namespace
+#kubectl wait --for=condition=Ready  pod/spark-client-0 -n $namespace  --timeout=120s
 for i in `seq $startingTenantId $lastTenantId`
 do
   #nrOfPartitions=$(($i * 2))
@@ -52,4 +52,4 @@ do
   period=$(($t2 - $t1))
   #if [ $period -lt 120 ]; then echo "sleeping for 5400 sec"; sleep 5400; fi	 
 done
-kubectl delete  -f spark-client/ -n $namespace
+#kubectl delete  -f spark-client/ -n $namespace
