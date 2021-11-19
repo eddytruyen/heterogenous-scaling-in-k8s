8eb4079488d5680954e4ef30b4fa37fab1def3ec
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-11-19 15:25:34 +0000

    test13.sh

--- apps/matrix-generator/test13.sh
+++ apps/matrix-generator/test13.sh
@@ -10,4 +10,12 @@ curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190
 sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3" | jq '.'
 sleep 1
-
+curl 'http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_0_1_3' | jq '.'
+sleep 1
+curl 'http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_0_3' | jq '.'
+sleep 1
+curl 'http://172.17.13.119:80/conf?namespace=silver&tenants=3' | jq '.'
+sleep 1
+curl 'http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_1_2' | jq '.'
+sleep 1
+curl 'http://172.17.13.119:80/conf?namespace=silver&tenants=3' | jq '.'
