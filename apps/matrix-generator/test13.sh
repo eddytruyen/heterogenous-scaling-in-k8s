6702b59b74f0b3cb06caec5b93121cf8f8d068f5
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2021-11-19 14:42:42 +0000

    update

--- apps/matrix-generator/test13.sh
+++ apps/matrix-generator/test13.sh
@@ -1,24 +1,13 @@
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=1" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_0_0" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=1&previousconf=0_1_0_0" | jq '.'
 sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=190&previoustenants=2&previousconf=0_0_1_2"| jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=190&previoustenants=2&previousconf=0_1_1_1" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=160&previoustenants=3&previousconf=0_1_1_2" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=190&previoustenants=3&previousconf=0_0_2_2" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=190&previoustenants=3&previousconf=0_1_0_3" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3&completiontime=120&previoustenants=3&previousconf=1_0_0_2" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_0_1_3" | jq '.'
-sleep 1
-curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_0_3" | jq '.'
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_0_1_2"| jq '.'
 sleep 1
 curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3" | jq '.'
+sleep 1
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=2&completiontime=190&previoustenants=2&previousconf=0_1_1_1" | jq '.'
+sleep 1
+curl "http://172.17.13.119:80/conf?namespace=silver&tenants=3" | jq '.'
+sleep 1
 
