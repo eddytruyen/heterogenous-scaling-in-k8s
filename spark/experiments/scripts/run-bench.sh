169815fba260f6b2378dc7f015e06839d5e87d4b
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2022-06-28 11:29:05 +0000

    Master without kube-proxy

--- spark/experiments/scripts/run-bench.sh
+++ spark/experiments/scripts/run-bench.sh
@@ -11,7 +11,7 @@ rm -r results-$workload-g$tenantgroup-t-*.csv
 mr=`cat /etc/podinfo/mem_request`
 exec_overhead_gb=$(($mr / 10))
 exec_mem=$(($mr - ${exec_overhead_gb}))
-sed "s/executor-memory = \".*G\"/executor-memory = \"${exec_mem}G\"/g" output.conf | sed "s/Release-master-svc.Namespace/$releasename-master-svc.$namespace/g" > tmp.conf
+sed "s/executor-memory = \".*G\"/executor-memory = \"${exec_mem}G\"/g" output.conf | sed "s/Release-master-svc/$releasename-master-svc/g" > tmp.conf
 exec_overhead_mb=$((${exec_overhead_gb} * 1024))
 sed  -i "s/spark.executor.memoryOverhead = \".*\"/spark.executor.memoryOverhead = \"${exec_overhead_mb}\"/g" tmp.conf
 ./bin/spark-bench.sh tmp.conf  2> run.log
