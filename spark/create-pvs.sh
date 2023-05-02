e19fab250b9160eb472a30ebe96bdf28263e42f4
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2023-05-02 11:02:18 +0000

    updaTE

--- spark/create-pvs.sh
+++ spark/create-pvs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 namespace=${1:-default}
+name=${2:-volume}
+round=$3
 echo $namespace
-kubectl create -f storageclass.yaml -n $namespace
+kubectl create -f storageclass.yaml
 count=`kubectl get nodes | wc -l`
 nodes=`expr $count - 2`
-for i in `seq $nodes`; do sed "s/local-pv/local-pv-$namespace-$i/g" persistentvolume.yaml > pv.yaml; kubectl create -f pv.yaml; done
+for i in `seq $nodes`; do sed "s/local-pv/local-pv-$namespace-$round-$i/g" persistent$name.yaml > pv.yaml; kubectl create -f pv.yaml; done
