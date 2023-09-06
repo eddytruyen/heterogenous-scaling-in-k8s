f93e6cf87355a75225dbf1f5e8b1c35dd7512ae5
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2023-09-06 15:08:08 +0000

    update

--- kubeadm/install_cluster.sh
+++ kubeadm/install_cluster.sh
@@ -12,14 +12,14 @@ for i in `echo $nodes`; do
 	ssh -i ~/.ssh/756245.pem ${subnet}.$i 'sudo kubeadm reset -f; ./install_kubeadm_worker.sh `cat ./k8s_version`; ./preinstall_weave.sh'; 
 	worker_count=$((worker_count + 1))
 done
-#sudo  kubeadm init --config kubeadm-config.yaml
-#mkdir -p $HOME/.kube
-#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
-#kubectl apply -f weave-daemonset-k8s.yaml
-for i in `echo $nodes`; do
-       ssh -i ~/.ssh/756245.pem ${subnet}.$i 'sudo kubeadm join 172.22.8.106:6443 --token tnpehh.rru6r40ku522mlbz \
-        --discovery-token-ca-cert-hash sha256:5e71f078e4bc79de4390ea3ac8105765f778137edac5aa0ba2c57246a171e5fe'
-done
+sudo  kubeadm init --config kubeadm-config.yaml
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f weave-daemonset-k8s.yaml
+#for i in `echo $nodes`; do
+#       ssh -i ~/.ssh/756245.pem ${subnet}.$i 'sudo kubeadm join 172.22.8.106:6443 --token tnpehh.rru6r40ku522mlbz \
+#        --discovery-token-ca-cert-hash sha256:5e71f078e4bc79de4390ea3ac8105765f778137edac5aa0ba2c57246a171e5fe'
+#done
 rm worker_count
 rm k8s_version
--- kubeadm/run.sh
+++ kubeadm/run.sh
@@ -2,10 +2,6 @@ k8s_version="1.27.5-00"
 subnet="172.22.8"
 worker_nodes="182 65"
 worker_start_index=1
-fresh_cluster=1
-if [ -eq $fresh_cluster 1 ]; 
-then
-	./install_kubeadm_master.sh
-	./preinstall_weave.sh
-fi
-./install_cluster.sh $k8s_version $subnet $worker_nodes $worker_start_index $fresh_cluster
+./install_kubeadm_master.sh
+./preinstall_weave.sh
+./install_cluster.sh $k8s_version $subnet $worker_nodes $worker_start_index
