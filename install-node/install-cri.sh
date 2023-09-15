c00b39b932d5010c9346d76b05dcb215bd9a8d66
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2023-09-15 20:14:59 +0000

    update

--- install-node/install-cri.sh
+++ install-node/install-cri.sh
@@ -1,15 +1,15 @@
 purge=${1:-0}
 
-if [ -eq $purge 1 ] 
-then
-	./purge-cri.sh
-else
-	./pre-cri.sh
-fi
+#if [ -eq $purge 1 ] 
+#then
+./purge-cri.sh
+#else
+#./pre-cri.sh
+#fi
 
-./pre-net.sh
+#./pre-net.sh
 ./install-containerd.sh
-./install-cni-plugins.sh
+#./install-cni-plugins.sh
 
 
 
