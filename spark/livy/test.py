ec3144459ac03dc110963162132f7de2228b41ad
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2023-09-20 13:49:34 +0000

    test.py

--- spark/livy/test.py
+++ spark/livy/test.py
@@ -2,18 +2,29 @@ import requests
 import json
 import random
 import pprint
+import textwrap
 
 # Definieer de URL van de Livy-server
 host = "http://172.22.8.106:30898"
 
-data = {'kind': 'pyspark'}
+response = requests.get(f"{host}/sessions", headers={'Content-Type': 'application/json'})
+sessions = response.json()['sessions']
+
 headers = {'Content-Type': 'application/json'}
-r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
-r.json()
+
+if len(sessions) > 0:
+  # Gebruik de eerste actieve sessie
+  session_id = sessions[0]['id']
+else:
+  data = {'kind': 'pyspark'}
+  #  {{ .Files.Get "livy.conf" | indent 4 }}
+  r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
+  session_id = r.json()['id']
+
 
 
 # Wacht tot de sessie actief is
-session_url = host + r.headers['location']
+session_url = f"{host}/sessions/{session_id}"
 r = requests.get(session_url, headers=headers)
 status = r.json()['state']
 while status != 'idle':
@@ -21,28 +32,19 @@ while status != 'idle':
   status = r.json()['state']
   print(status)
 
-import pdb; pdb.set_trace()
-# Definieer de naam van de tabel
-table_name = "spark_data/spark-bench-test/kmeans-data-g7-1.csv"
+table_name = "file:///opt/bitnami/spark/spark_data/spark-bench-test/kmeans-data-g7-1.csv"
 
-# Lees de tabel in als een DataFrame
-command = textwrap.dedent("""
-df = spark.read.format("csv").option("header", "true").load(table_name)
-""")
-requests.post(f"{livy_url}/sessions/{session_id}/statements", data=json.dumps({'code': command}), headers=headers)
+command = """
+df = spark.read.format("csv").option("header", "true").load("file:///opt/bitnami/spark/spark_data/spark-bench-test/kmeans-data-g7-1.csv")
+df.show()
+"""
+response=requests.post(f"{host}/sessions/{session_id}/statements", data=json.dumps({'code': command}), headers=headers)
 
-# Genereer willekeurig een nummer tussen 1 en 3
-operation = random.randint(1, 3)
-
-if operation == 1:
-  # Selecteer willekeurig een kolom
-  column = random.choice(df.columns)
-  # Genereer de SQL-query om de geselecteerde kolom te selecteren
-  command = f"query = f'SELECT {column} FROM {table_name}'"
-
-elif operation == 2:
-  # Selecteer willekeurig twee kolommen
-  columns = random.sample(df.columns, 2)
-  # Genereer de SQL-query om de geselecteerde kolomm
-  command = f"query = f'SELECT {column} FROM {table_name}'"
+statement_url = host + response.headers['Location']
+r = requests.get(statement_url, headers=headers)
+status=(r.json())['progress']
+while status != 1.0:
+    r = requests.get(statement_url, headers=headers)
+    status=(r.json())['progress']
+pprint.pprint(r.json())
 
