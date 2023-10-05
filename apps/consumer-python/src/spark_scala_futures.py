8bb85a0e90b485e10016a905f05218bbafaa1d56
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2023-10-05 14:00:53 +0000

    spark-integrated-into-abel

---
+++ apps/consumer-python/src/spark_scala_futures.py
@@ -0,0 +1,65 @@
+import concurrent.futures
+import requests
+import json
+import random
+import pprint
+import textwrap
+
+
+max_tenants=15
+tenant_group="g7"
+host = "http://172.22.8.106:30898"
+headers = {'Content-Type': 'application/json'}
+
+def invoke(command, host, session_id,headers):
+
+    def make_request():
+        response = requests.post(f"{host}/sessions/{session_id}/statements", data=json.dumps({'code': command}), headers=headers)
+        statement_url = host + response.headers['Location']
+        r = requests.get(statement_url, headers=headers)
+        status = r.json()['state']
+        while status != "available":
+            r = requests.get(statement_url, headers=headers)
+            status = r.json()['state']
+        return r.json()
+    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(make_request)
+        result = future.result()
+    
+    return result
+
+
+def get_session(host, headers): 
+   response = requests.get(f"{host}/sessions", headers=headers)
+   sessions = response.json()['sessions']
+
+   session_id=0
+
+   active_sessions=[]
+   if len(sessions) > 0:
+      # Gebruik de eerste actieve sessie
+      active_sessions = list(filter(lambda x: x['kind'] == 'spark' and x['state'] != 'dead',sessions))
+
+      if len(active_sessions) > 0:
+         for active_session in active_sessions:
+            if active_session['kind']=='spark':
+               session_id=active_session['id']
+               break
+   else:
+      data = {'kind': 'spark', 'executorMemory': 6442450944, 'executorCores': 4, 'proxyUser': 'ubuntu'}
+      r = requests.post(host + '/sessions', data=json.dumps(data), headers=headers)
+      session_id = r.json()['id']
+
+
+
+   # Wacht tot de sessie actief is
+   session_url = f"{host}/sessions/{session_id}"
+   r = requests.get(session_url, headers=headers)
+   status = r.json()['state']
+   while status == 'starting':
+      r = requests.get(session_url, headers=headers)
+      status = r.json()['state']
+   return session_id
+
+
--- apps/consumer-python/src/stress.py
+++ apps/consumer-python/src/stress.py
@@ -1,20 +1,72 @@
 import math
+import random
+import textwrap
 
-def _cpu_stress(stress_size):
-	for i in range(1000*stress_size):
-		math.sqrt(i)
+
+max_tenants=15
+tenant_group="g7"
+host = "http://172.22.8.106:30898"
+headers = {'Content-Type': 'application/json'}
+
+
+
+def _stress(stress_size):
+
+  session_id=get_session(self.host, self.headers)
+
+
+# Genereer willekeurig een nummer tussen 1 en max_tenants
+  tenant = str(random.randint(1, self.max_tenants))
+
+  table_name = f"file:///opt/bitnami/spark/spark_data/spark-bench-test/kmeans-data-{tenant_group}-{tenant}.csv"
+
+  command =textwrap.dedent(f"""
+  val df = spark.read.format("csv").option("header", "true").load("{table_name}")
+  df.createOrReplaceTempView("kmeans{tenant}")
+  df.cache()
+  val e = df.columns
+  %json e
+  """)
+
+
+  r=invoke(command, self.host, session_id, self.headers)
+  columns=r['output']['data']['application/json']
+  sample_columns=random.sample(columns, random.randint(0, len(columns)-1))
+  selected_columns = ", ".join(sample_columns)
+
+
+  sample_queries=[]
+  if stress_size == 0:
+    sample_queries=random.sample(columns, random.randint(0, len(columns)-1))
+  else:
+    sample_queries=random.sample(columns, stress_size)
+  sample_values=[random.uniform(-0.1, 0.1) for _ in sample_queries]
+
+  selected_evaluations= " and ".join([f"{string} < {value}" for string, value in zip(sample_queries,sample_values)])
+
+  command = textwrap.dedent(f"""
+  val sqlDF = spark.sql("SELECT {selected_columns} FROM kmeans{tenant} WHERE {selected_evaluations}")
+  sqlDF.show()
+  """)
+  #sqlDF.write.mode("overwrite").csv("file:///opt/bitnami/spark/spark_data/spark-bench-test/output/output.csv")
+
+  invoke(command, self.host, session_id, self.headers)
 
 class Stress:
-	def __init__(self, stress_size,stress_function):
+	def __init__(self, stress_size,stress_function, max_tenants, tenant_group, host, headers):
 		self.stress_size = stress_size
 		self.stress_function = stress_function
+        self.max_tenants=max_tenants
+        self.tenant_group=tenant_group
+        self.host=host
+        self.headers=headers
+
 
 	def runTest(self):
-		if self.stress_size != 0:
-			self.stress_function(self.stress_size)
+		self.stress_function(self.stress_size)
 
 class StressCPU(Stress):
-	def __init__(self,stress_function=_cpu_stress,stress_size=100):
+	def __init__(self,stress_function=stress,stress_size=100, max_tenants=15, tenant_group="g7", host = "http://172.22.8.106:30898", headers = {'Content-Type': 'application/json'}):
 		Stress.__init__(self, stress_size, stress_function)
 
 
