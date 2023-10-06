83c513cd6053fa2f300e9ef42bdf10b311c4bb57
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2023-10-06 16:40:54 +0000

    update

--- apps/java-application/demo/src/main/java/com/matthijs/demo/UserCount.java
+++ apps/java-application/demo/src/main/java/com/matthijs/demo/UserCount.java
@@ -7,9 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.stereotype.Component;
 import java.util.UUID;
 
+@Component
 public class UserCount {
 
     @Autowired
