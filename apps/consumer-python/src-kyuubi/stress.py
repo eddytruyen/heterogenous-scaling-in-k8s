f856102138fbe0a9c39e48fa99aed835c5ff2bac
Eddy Truyen <eddy.truyen@cs.kuleuven.be>
2023-11-09 14:22:48 +0000

    update

--- apps/consumer-python/src-kyuubi/stress.py
+++ apps/consumer-python/src-kyuubi/stress.py
@@ -1,20 +1,24 @@
 import math
 import random
 import jaydebeapi
+import logging
 
 
-def _stress(stress_size, max_tenants, tenant_group, host,conn):
+def _stress(stress_size, max_tenants, tenant_group, conn):
 
 
 
 # Genereer willekeurig een nummer tussen 1 en max_tenants
   tenant = str(random.randint(1, max_tenants))
 
+  logging.debug("Creating JDBC statement for Kyuubi")
+
   cursor = conn.cursor()
 
   sql=f"select c1,c2,c3 from global_temp.kmeans{tenant} where c1 < 1 and c2 < 1 and c3 < 5"
-
+  logging.debug("Sending sql query to Kyuubi")
   cursor.execute(sql)
+  results=cursor.fetchall()
   cursor.close()
    
   #columns=r['output']['data']['application/json']
@@ -37,21 +41,20 @@ def _stress(stress_size, max_tenants, tenant_group, host,conn):
   #invoke(command, host, session_id, headers)
 
 class Stress:
-    def __init__(self, stress_size,stress_function, max_tenants, tenant_group, host, headers):
+    def __init__(self, stress_size,stress_function, max_tenants, tenant_group, conn):
         self.stress_size = stress_size
         self.stress_function = stress_function
         self.max_tenants=max_tenants
         self.tenant_group=tenant_group
-        self.host=host
-        self.headers=headers
+        self.conn=conn
 
 
     def runTest(self):
-        self.stress_function(self.stress_size, self.max_tenants, self.tenant_group, self.host, self.headers)
+        self.stress_function(self.stress_size, self.max_tenants, self.tenant_group, self.conn)
 
 class StressSpark(Stress):
-	def __init__(self,stress_size=0, max_tenants=15, tenant_group="g7", host = "http://172.22.8.106:30898", headers = {'Content-Type': 'application/json'}, stress_function=_stress):
-		Stress.__init__(self, stress_size, stress_function,max_tenants, tenant_group,host, headers)
+	def __init__(self,stress_size=0, max_tenants=15, tenant_group="g7", conn=None, stress_function=_stress):
+		Stress.__init__(self, stress_size, stress_function,max_tenants, tenant_group,conn)
 
 
 
